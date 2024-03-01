370ab9ac10beb1e7dcf2c46ec8fd368b61f90271
jltorres29 <jltorres29@hotmail.com>
2024-03-01 12:38:00 +0100

    home

--- src/components/Job/Jobs.jsx
+++ src/components/Job/Jobs.jsx
@@ -4,46 +4,57 @@ import 'slick-carousel/slick/slick-theme.css';
 
 import { Link } from "react-router-dom";
 
-import Carer from "../../assets/gifWorks/carer-cuidador.gif";
-import Carpenter from "../../assets/gifWorks/carpenter.gif";
-import Brickwork from "../../assets/gifWorks/brickwork-reformas-en-general.gif";
-import Chef from "../../assets/gifWorks/chef.gif";
-import ClosetOrganizer from "../../assets/gifWorks/closet-organizer-organizador-armario.gif";
-import Electrician from "../../assets/gifWorks/electrician.gif";
-import Fitter from "../../assets/gifWorks/fitter-montador-muebles.gif";
-import Gardener from "../../assets/gifWorks/gardener-jardinero.gif";
-import HomeCleaner from "../../assets/gifWorks/home-cleaner-limpieza-hogar.gif";
-import Locksmith from "../../assets/gifWorks/locksmith-cerrajero.gif";
-import Messenger from "../../assets/gifWorks/messenger-hacer-compra.gif";
-import Painter from "../../assets/gifWorks/painter-pintor.gif";
-import Plumber from "../../assets/gifWorks/plumber.gif";
-import Teacher from "../../assets/gifWorks/teacher.gif";
-import Welder from "../../assets/gifWorks/welder-soldador.gif";
+import CarerGif from "../../assets/gifWorks/carer-cuidador.gif";
+import CarpenterGif from "../../assets/gifWorks/carpenter.gif";
+import BrickworkGif from "../../assets/gifWorks/brickwork-reformas-en-general.gif";
+import ChefGif from "../../assets/gifWorks/chef.gif";
+import ClosetOrganizerGif from "../../assets/gifWorks/closet-organizer-organizador-armario.gif";
+import ElectricianGif from "../../assets/gifWorks/electrician.gif";
+import FitterGif from "../../assets/gifWorks/fitter-montador-muebles.gif";
+import GardenerGif from "../../assets/gifWorks/gardener-jardinero.gif";
+import HomeCleanerGif from "../../assets/gifWorks/home-cleaner-limpieza-hogar.gif";
+import LocksmithGif from "../../assets/gifWorks/locksmith-cerrajero.gif";
+import MessengerGif from "../../assets/gifWorks/messenger-hacer-compra.gif";
+import PainterGif from "../../assets/gifWorks/painter-pintor.gif";
+import PlumberGif from "../../assets/gifWorks/plumber.gif";
+import TeacherGif from "../../assets/gifWorks/teacher.gif";
+import WelderGif from "../../assets/gifWorks/welder-soldador.gif";
+
+import Carer from "../../assets/pngWorks/carer-cuidador.png"
+
 import "./Jobs.css";
+import { useState } from "react";
+
+
 
 const info = [
-  { src: Carer, text: "Carer", to: "Carer" },
-  { src: Carpenter, text: "Carpenter", to: "Carpenter" },
-  { src: Brickwork, text: "Brickwork", to: "Brickwork" },
-  { src: Chef, text: "Chef", to: "Chef" },
-  { src: ClosetOrganizer, text: "ClosetOrganizer", to: "ClosetOrganizer" },
-  { src: Electrician, text: "Electrician", to: "Electrician" },
-  { src: Fitter, text: "Fitter", to: "Fitter" },
-  { src: Gardener, text: "Gardener", to: "Gardener" },
-  { src: HomeCleaner, text: "HomeCleaner", to: "HomeCleaner" },
-  { src: Locksmith, text: "Locksmith", to: "Locksmith" },
-  { src: Messenger, text: "Messenger", to: "Messenger" },
-  { src: Painter, text: "Painter", to: "Painter" },
-  { src: Plumber, text: "Plumber", to: "Plumber" },
-  { src: Teacher, text: "Teacher", to: "Teacher" },
-  { src: Welder, text: "Welder", to: "Welder" }
+  { gifSrc: CarerGif, text: "Carer", to: "Carer" },
+  { staticSrc: Carer, text: "Carer", to: "Carer" },
+  { src: CarpenterGif, text: "Carpenter", to: "Carpenter" },
+  { src: BrickworkGif, text: "Brickwork", to: "Brickwork" },
+  { src: ChefGif, text: "Chef", to: "Chef" },
+  { src: ClosetOrganizerGif, text: "ClosetOrganizer", to: "ClosetOrganizer" },
+  { src: ElectricianGif, text: "Electrician", to: "Electrician" },
+  { src: FitterGif, text: "Fitter", to: "Fitter" },
+  { src: GardenerGif, text: "Gardener", to: "Gardener" },
+  { src: HomeCleanerGif, text: "HomeCleaner", to: "HomeCleaner" },
+  { src: LocksmithGif, text: "Locksmith", to: "Locksmith" },
+  { src: MessengerGif, text: "Messenger", to: "Messenger" },
+  { src: PainterGif, text: "Painter", to: "Painter" },
+  { src: PlumberGif, text: "Plumber", to: "Plumber" },
+  { src: TeacherGif, text: "Teacher", to: "Teacher" },
+  { src: WelderGif, text: "Welder", to: "Welder" }
 ]
 
-const CarouselItem = ({ src, text, to }) => {
+const CarouselItem = ({ gifSrc, staticSrc, text, to }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="jobs-line">
+    <div className="jobs-line" onMouseEnter={() => setIsHovered(true)}
+    onMouseLeave={() => setIsHovered(false)}>
+
        <Link to={`/job/${to}`}>
-        <img src={src} />
+        <img src={isHovered ? gifSrc : staticSrc} alt={text} />
         <p>{text}</p>
       </Link>
     </div>
@@ -65,7 +76,7 @@ const Jobs = () => {
       <div className="custom-carousel">
         <Slider {...carousel}>
           {info.map((job, index) => (
-            <CarouselItem key={index} src={job.src} text={job.text} to={job.to} />
+            <CarouselItem key={index} gifSrc={job.src} staticSrc={job.staticSrc} text={job.text} to={job.to} />
           ))}
         </Slider>
       </div>
