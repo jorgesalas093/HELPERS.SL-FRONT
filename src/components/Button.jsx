5c44609f1cc1432b40026f724e97acc26a65d2f9
jorgesalas093 <jorgesalas093@gmail.com>
2024-03-12 02:47:53 +0100

    edit try, not finish

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,16 @@
 import { useState } from 'react';
+import { TbPhotoEdit } from "react-icons/tb";
+import { RiDeleteBinLine } from "react-icons/ri";
+import { GrFormEdit } from "react-icons/gr";
+import { BsSend } from "react-icons/bs";
+import { IoChatbubblesOutline } from "react-icons/io5";
+import { FaRegCommentDots } from "react-icons/fa6";
+import { GrDocumentUpdate } from "react-icons/gr";
+<GrDocumentUpdate />
 
-const Button = ({ type, onClick, text, disabled, purpose }) => {
+const Button = ({ type, onClick, text, disabled, purpose, color }) => {
   const [isClicked, setIsClicked] = useState(false);
+
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
       onClick();
@@ -10,34 +19,56 @@ const Button = ({ type, onClick, text, disabled, purpose }) => {
 
   const handleClick = () => {
     setIsClicked(!isClicked);
-    onClick();
+    if (onClick) {
+      onClick();
+    }
   };
 
-  //TOCA HACER BIEN LA LOGICA DE LOS ICONOS, PREGUNTAR A PABLO
-  //PREGUNTAR A PABLO TAMBIEN PORQUE NO FUNCIONA EL event.key AL PULSAR ENTER
+
   const getIcon = (purpose) => {
     switch (purpose) {
       case 'delete':
-        return <i className="fas fa-trash-alt"></i>; // Cambia el icono de acuerdo al propósito
+        return <RiDeleteBinLine />;
+      case 'editphoto':
+        return <TbPhotoEdit />;
       case 'edit':
-        return <i className="fas fa-edit"></i>; // Cambia el icono de acuerdo al propósito
+        return <GrFormEdit />;
+      case 'chat':
+        return <IoChatbubblesOutline />;
+      case 'send':
+        return <BsSend />;
+      case 'comment':
+        return <FaRegCommentDots />;
+      case 'save':
+        return <GrDocumentUpdate />;
       default:
         return null;
+    }
+  };
+
+  const getColor = (color) => {
+    switch (color) {
+      case 'red':
+        return 'bg-red-400'; // Cambia el icono de acuerdo al propósito
+      case 'green':
+        return 'bg-green-400'; // Cambia el icono de acuerdo al propósito
+      default:
+        return 'bg-he-primary';
     }
   };
 
   return (
     <div tabIndex={0} onKeyDown={handleKeyDown}>
       <button
-          type={type}
-          onClick={handleClick}
-          onKeyDown={handleKeyDown}
-          tabIndex={0} // Permite que el botón reciba el foco, pero no se porque no se ejecuta cuando se pulsa 'Enter'
-          className={`text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center 
-          ${!disabled && !isClicked ? 'bg-he-primary' : ''}
+        type={type}
+        onClick={handleClick}
+        onKeyDown={handleKeyDown(event)}
+        tabIndex={0} // Permite que el botón reciba el foco, pero no se porque no se ejecuta cuando se pulsa 'Enter'
+        className={`text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center 
+          ${!disabled && !isClicked ? getColor(color) : ''}
           ${!disabled && isClicked ? 'bg-blue-800' : ''}
           ${disabled ? 'bg-gray-400' : ''}`}
-        >
+      >
         {getIcon(purpose)} {/* Renderiza el icono basado en el propósito */}
         {text}
       </button>
